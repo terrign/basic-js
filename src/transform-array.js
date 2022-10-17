e58cc39a39a1009642c37057945d668804e6301a
terrign <vajipi@gmail.com>
2022-10-17 18:07:06 +0300

    commit

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if(!Array.isArray) throw new Error("'arr' parameter must be an instance of the Array!")
+  let arrayClone = []; arr.forEach(a => arrayClone.push(a))
+  let params = ['--discard-next','--discard-prev','--double-next','--double-prev']
+  const findArrayParam = () => {
+      let paramID = -1
+      let paramArrayIndex = -1
+      params.forEach((a,i) => {
+          if (arrayClone.includes(a)) {
+              paramID = i
+              paramArrayIndex = arrayClone.indexOf(a)
+          }
+      })
+      return paramArrayIndex == -1 || paramID == -1? false : [paramID,paramArrayIndex]
+  }
+  while (findArrayParam()) {
+
+      let ids = findArrayParam()
+      switch (ids[0]) {
+          case 0: arrayClone.splice(ids[1], 2); break;
+          case 1: ids[1] == 0 ? arrayClone.splice(0,1) : arrayClone.splice(ids[1] - 1, 2); break;
+          case 2: ids[1] == arrayClone.length -1 ? arrayClone.splice(ids[1],1) : arrayClone.splice(ids[1],1, arrayClone[ids[1] + 1]); break;
+          case 3: ids[1] == 0 ? arrayClone.splice(0,1) : arrayClone.splice(ids[1],1, arrayClone[ids[1] - 1]); break;
+      }
+  }
+  return arrayClone
 }
 
 module.exports = {
