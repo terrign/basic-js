edb2989f252260e36c34f1b3461c836ac087e568
terrign <vajipi@gmail.com>
2023-04-17 16:36:07 +0300

    vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (bool) {
+      this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+      this.isReverse = bool === false ? true : false
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message,key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase().split('')
+    let keyString = key.toUpperCase().repeat(Math.ceil(message.length/key.length)).slice(0,message.length).split('')
+    let result =  message.map((sym,index) => {
+      if (this.alphabet.indexOf(sym) === -1) {
+          keyString.splice(index, 0, sym)
+          return sym
+      }
+      let encryptedLeterNumber = (this.alphabet.indexOf(sym) + this.alphabet.indexOf(keyString[index])) % 26
+      return this.alphabet[encryptedLeterNumber]
+    })
+    return this.isReverse ? result.reverse().join('') : result.join('')
+  }
+
+  decrypt(message,key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase().split('')
+    let keyString = key.toUpperCase().repeat(Math.ceil(message.length/key.length)).slice(0,message.length).split('')
+    let result =  message.map((sym,index) => {
+      if (this.alphabet.indexOf(sym) === -1) {
+        keyString.splice(index, 0, sym)
+        return sym
+      }
+      let decryptedLeterNumber = (this.alphabet.indexOf(sym) + 26 - this.alphabet.indexOf(keyString[index])) % 26
+      return this.alphabet[decryptedLeterNumber]
+    })
+    return this.isReverse ? result.reverse().join('') : result.join('')
   }
 }
 
